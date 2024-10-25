574b56aa70a895639f216d202ce7c3475fb9d0b5
robertSpacecraft <robertspacecraft@gmail.com>
2024-10-25 08:31:56 +0200

    Añadir archivos del proyecto oficina y configuraciones

---
+++ src/programacionProblemas/programacionProblemas.java
@@ -0,0 +1,21 @@
+package programacionProblemas;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Muestras
+ */
+public class programacionProblemas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
