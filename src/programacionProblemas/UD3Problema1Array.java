b8ab54c2e462a782e6e69e7f612cf0d4320e6b44
robertSpacecraft <robertspacecraft@gmail.com>
2024-10-25 11:28:11 +0200

    Creado el archivo y declarado scanner

---
+++ src/programacionProblemas/UD3Problema1Array.java
@@ -0,0 +1,12 @@
+package programacionProblemas;
+
+//Roberto Amorós Linares
+
+import java.util.Scanner;
+
+public class UD3Problema1Array {
+    public static void main(String[] args) {
+        
+    }
+    
+}
