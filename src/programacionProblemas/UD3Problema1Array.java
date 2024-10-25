bdc46b9008ddddd545c3629a491ba87edba53076
robertSpacecraft <robertspacecraft@gmail.com>
2024-10-25 17:44:59 +0200

    Añadido un String y nuevo planteamineto

--- src/programacionProblemas/UD3Problema1Array.java
+++ src/programacionProblemas/UD3Problema1Array.java
@@ -7,7 +7,29 @@ import java.util.Scanner;
 public class UD3Problema1Array {
     public static void main(String[] args) {
         //Creamos las variables
+        float[] sistoDiasto = new float[10];
+        String lineaDatos;
         
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Introduzca los datos: ");
+        lineaDatos = input.nextLine();
+        
+        
+        
+        System.out.print("Introduce los valores Sistólica y Diástolica: ");
+        for (int i = 0; i < sistoDiasto.length; i++) {
+            
+          
+            
+        }
+        
+        for (int i = 0; i < sistoDiasto.length; i++) {
+            
+            
+            
+        }
+        System.out.println("Las medidas obtenidas son:" + lineaDatos);
     }
     
 }
