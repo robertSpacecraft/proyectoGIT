ea4372751f412b969982ca9dbfcb85c3d1709912
robertSpacecraft <robertspacecraft@gmail.com>
2024-10-30 20:57:45 +0100

    Update HolaMundo.java
    Cambiado hola mundo por hola git

--- HolaMundo.java
+++ HolaMundo.java
@@ -10,7 +10,7 @@ package programacionproblemas;
  */
 public class HolaMundo {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        System.out.println("Hola Git");
     }
     
 }
